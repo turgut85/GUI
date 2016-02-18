6a42278b1355f7ff0a0e871363427ddccfc93c4b
Josh Siegle <joshs@alleninstitute.org>
2016-02-17 20:26:57 -0800

    Clean up NeuropixThread.cpp

--- Source/Processors/DataThreads/NeuropixThread.cpp
+++ Source/Processors/DataThreads/NeuropixThread.cpp
@@ -61,56 +61,17 @@ NeuropixThread::NeuropixThread(SourceNode* sn) : DataThread(sn), baseStationAvai
 	EepromErrorCode error4 = neuropix.neuropix_readId(asicId);
 
 	std::cout << "  Hardware version number: " << hw_version.major << "." << hw_version.minor << std::endl;
-	std::cout << "  Basestation version number: " << bs_version << std::endl;
-	std::cout << "  Basestation revision number: " << bs_revision << std::endl;
+	std::cout << "  Basestation version number: " << String(bs_version) << "." << String(bs_revision) << std::endl;
 	std::cout << "  API version number: " << vn.major << "." << vn.minor << std::endl;
-//	std::cout << "  Asic info: " << asicId.probeType << std::endl;
+	std::cout << "  Asic info: " << asicId.probeType << std::endl;
 
 	dataBuffer = new DataBuffer(384, 10000); // start with 384 channels and automatically resize
-
-	//packet = new ElectrodePacket(); why doesn't this work???
 
 	// channel selections:
 	// Options 1 & 2 -- fixed 384 channels
 	// Option 3 -- select 384 of 960 shank electrodes
 	// Option 4 -- select 276 of 966 shank electrodes
 
-	// void neuropix_selectElectrodes(int chNum, int connection)
-	// chNum = 0 to 383
-	// connection = 0-2 for Option 3 [1 to 768, chNum + 384*connection]
-	//              0-3 for Option 4 [1 to 828, chNum + 276*chNum]
-
-	// int neuropix_setReference(int chNum, int refSetting)
-	//  Options 1, 2, and 3: ch 0-383, 0-10 refSetting
-	//  Option 4: 0-275, 0-7 refSetting
-	//  Each channel can have a separate reference input
-	//  
-
-	// int neuropix_setGain(int ch, int apGain, int lfpGain, bool standby)
-	//   ch = 0-383
-	//   apGain = 0-7
-	//   lfpGain = 0-7
-	//   standby = 0 or 1
-
-	// int neuropix_setFilter (int filter)
-	// 00 = 300 Hz
-	// 01 = 500 Hz
-	// 11 = 1 kHz
-
-	// int neuropix_setMode (unsigned char mode)
-	// Mode 2: impedance measurement, 3: recording
-
-	// status codes:
-	// 0 = success
-	// 1 = fail
-	// 2 = no device
-	// 3 = device info fail
-	// 4 = open fail
-	// 5 = FIFO fail
-	// 6 = write fail
-	// 7 = read fail
-	// 8 = no data to read
-	// 9 = shank option error
 }
 
 NeuropixThread::~NeuropixThread()
@@ -122,13 +83,16 @@ NeuropixThread::~NeuropixThread()
 /** Returns true if the data source is connected, false otherwise.*/
 bool NeuropixThread::foundInputSource()
 {
-	return true;
-	//return baseStationAvailable;
+	return baseStationAvailable;
 }
 
 /** Initializes data transfer.*/
 bool NeuropixThread::startAcquisition()
 {
+
+	// set into recording mode
+	neuropix.neuropix_mode(ASIC_RECORDING);
+
 	// clear the neuropix buffer
     neuropix.neuropix_nrst(false);
     neuropix.neuropix_resetDatapath();
@@ -138,8 +102,16 @@ bool NeuropixThread::startAcquisition()
 	dataBuffer->clear();
 
 	if (internalTrigger)
-	  ConfigAccessErrorCode caec = neuropix.neuropix_setNeuralStart();
+	{
+		ConfigAccessErrorCode caec = neuropix.neuropix_setNeuralStart();
 
+		if (caec != CONFIG_SUCCESS)
+		{
+			std::cout << "start failed with error code " << caec << std::endl;
+			return false;
+		}
+	}
+	  
 	startThread();
 
 	return true;
@@ -155,6 +127,7 @@ bool NeuropixThread::stopAcquisition()
 	}
 
 	neuropix.neuropix_nrst(false);
+
 	return true;
 }
 
@@ -196,34 +169,28 @@ int NeuropixThread::getNumEventChannels()
 
 void NeuropixThread::selectElectrode(int chNum, int connection)
 {
-	// ShankConfigErrorCode scec = neuropix_selectElectrode(chNum, connection);
-
-	// if (scec == SHANK_SUCCESS)
-	// {
-	// 	// all good!
-	// }
+	ShankConfigErrorCode scec = neuropix.neuropix_selectElectrode(chNum, connection);
 }
 
 void NeuropixThread::setReference(int chNum, int refSetting)
 {
-	// BaseConfigErrorCode bcec = neuropix_setReference(chNum, refSetting);
+	BaseConfigErrorCode bcec = neuropix.neuropix_setReference(chNum, refSetting);
 }
 
 void NeuropixThread::setGain(int chNum, int apGain, int lfpGain)
 {
-	// BaseConfigErrorCode bcec = neuropix_setGain(ch, apGain, lfpGain, 0);
+	BaseConfigErrorCode bcec = neuropix.neuropix_setGain(chNum, apGain, lfpGain);
 }
 
 void NeuropixThread::setFilter(int filter)
 {
-	// BaseConfigErrorCode bcec = neuropix_setFilter(filter);
+	BaseConfigErrorCode bcec = neuropix.neuropix_setFilter(filter);
 }
 
 void NeuropixThread::setTriggerMode(bool trigger)
 {
-	// ConfigAccessErrorCode caec = neuropix_triggermode(trigger);
+	ConfigAccessErrorCode caec = neuropix.neuropix_triggerMode(trigger);
 }
-
 
 bool NeuropixThread::updateBuffer()
 {
@@ -236,44 +203,37 @@ bool NeuropixThread::updateBuffer()
 	{
 		int64 timestamp = 0;
 		uint64 eventCode = 0;
-		float data[384] = { 0.0 };
 
 		for (int i = 0; i < 12; i++)
 		{
-			eventCode = (uint64)packet.synchronization[i];
-			timestamp = (int64)packet.ctrs[i][0];
-
-			for (int ch = 0; ch < 384; ch++)
-			{
-				//float dataPt = half_float::half_cast<float, half_float::half>(packet.apData[i][ch]);
-				data[ch] = packet.apData[i][ch];
-			}
+			eventCode = (uint64) packet.synchronization[i];
+			timestamp = (int64) packet.ctrs[i][0];
 			
-			dataBuffer->addToBuffer(data, &timestamp, &eventCode, 1);
+			dataBuffer->addToBuffer(packet.apData[i], &timestamp, &eventCode, 1);
 		}
 
-		std::cout << "READ SUCCESS!" << std::endl;
+		//std::cout << "READ SUCCESS!" << std::endl;
 	
 	}
 	else {
 		if (rec == NO_DATA_LINK)
 		{
-			std::cout << "NO DATA LINK" << std::endl;
+			//std::cout << "NO DATA LINK" << std::endl;
 		}
 		else if (rec == WRONG_DATA_MODE)
 		{
-			std::cout << "WRONG DATA MODE" << std::endl;
+			//std::cout << "WRONG DATA MODE" << std::endl;
 		}
 		else if (rec == DATA_BUFFER_EMPTY)
 		{
-			std::cout << "DATA BUFFER EMPTY" << std::endl;
+			//std::cout << "DATA BUFFER EMPTY" << std::endl;
 		}
 		else if (rec == DATA_ERROR)
 		{
-			std::cout << "DATA ERROR" << std::endl;
+			//std::cout << "DATA ERROR" << std::endl;
 		}
 		else {
-			std::cout << "ERROR CODE: " << rec << std::endl;
+			//std::cout << "ERROR CODE: " << rec << std::endl;
 		}
 	}
 	 
